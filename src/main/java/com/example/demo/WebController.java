f8ebc3682dc94d1a980a752d912038b23d6c9268
yoondgu <doyoungwork@gmail.com>
2023-09-28 17:48:01 +0900

    feat: Pin insert api 호출 구현

--- src/main/java/com/example/demo/WebController.java
+++ src/main/java/com/example/demo/WebController.java
@@ -2,13 +2,14 @@ package com.example.demo;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
-import com.example.geographicfileparser.PinCreateRequest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.tomcat.util.http.parser.Authorization;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -17,40 +18,68 @@ import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 public class WebController {
 
+    private static ObjectMapper objectMapper = new ObjectMapper();
+    private final PinService pinService;
+    private String accessToken;
+
+    public WebController(
+            @Value("${authorization.access-token}")
+            String accessToken,
+            PinService pinService
+    ) {
+        this.accessToken = accessToken;
+        this.pinService = pinService;
+    }
+
     @PostMapping("/pins")
-    public void insert() throws JsonProcessingException {
+    public void insert() throws Exception {
         URI url = UriComponentsBuilder.fromHttpUrl("https://mapbefine.kro.kr/api/pins")
                 .build()
                 .toUri();
-        ObjectMapper objectMapper = new ObjectMapper();
-
         WebClient webClient = WebClient.builder().build();
 
-        System.out.println(url);
-        String pinJson = objectMapper.writeValueAsString(new PinCreateRequest(551730L, "붕어빵테스트", "붕어빵테스트 description", "붕어빵 테스트 address", "temp", 35.172544, 129.136106));
-        HttpStatusCode statusCode = webClient.post()
-                .uri(url)
-                /// TODO: 2023/09/28  토큰 직접 입력하기
-                .header(AUTHORIZATION, "token")
-                .contentType(MediaType.MULTIPART_FORM_DATA)
-                /// TODO: 2023/09/28 multipart 넣는 다른 방식 찾아봐야 할듯
-                .body(BodyInserters.fromMultipartData("request", pinJson))
-                .exchangeToMono(response -> {
-                    HttpStatusCode httpStatusCode = response.statusCode();
-                    try {
-                        System.out.println(httpStatusCode.value());
-                        return response.bodyToMono(HttpStatus.class).thenReturn(httpStatusCode);
-                    } catch (Exception exception) {
-                        System.out.println(httpStatusCode.value());
-                        throw new IllegalStateException(exception);
-                    }
-                })
-                .block();
+        List<PinCreateRequest> pins = pinService.parsePins("붕어빵.txt");
+        pins.forEach(pin -> {
+            try {
+                sendPost(url, webClient, pin);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    private void sendPost(URI url, WebClient webClient, PinCreateRequest pinCreateRequest) throws JsonProcessingException {
+        MultiValueMap<String, HttpEntity<?>> multipartBody = buildMultipartBody(pinCreateRequest);
+        System.out.println(multipartBody.get("request"));
+
+        try {
+            Mono<String> responseBody = webClient.post()
+                    .uri(url)
+                    .header(AUTHORIZATION, "Bearer " + accessToken)
+                    .contentType(MediaType.MULTIPART_FORM_DATA)
+                    .body(BodyInserters.fromMultipartData(multipartBody))
+                    .retrieve()
+                    .onStatus(
+                            HttpStatus.INTERNAL_SERVER_ERROR::equals,
+                            response -> response.bodyToMono(String.class).map(Exception::new))
+                    .bodyToMono(String.class);
+            System.out.println(responseBody.block());
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
 
         System.out.println("done");
+    }
+
+    private MultiValueMap<String, HttpEntity<?>> buildMultipartBody(PinCreateRequest pinCreateRequest) throws JsonProcessingException {
+        String pinJson = objectMapper.writeValueAsString(pinCreateRequest);
+        MultipartBodyBuilder multipartBodyBuilder = new MultipartBodyBuilder();
+        multipartBodyBuilder.part("request", pinJson, MediaType.APPLICATION_JSON);
+        return multipartBodyBuilder.build();
     }
 }
